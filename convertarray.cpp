4345ad2169250409ba8954cf6b5976cef28e6126
sahilghai87 <sahilghai.iter@gmail.com>
2024-10-10 11:21:17 +0530

    Update convertarray.cpp

--- convertarray.cpp
+++ convertarray.cpp
@@ -19,7 +19,7 @@
 #include <ranges>
 #include <algorithm>
 
-#define fwd(arg) std::forward<decltype(arg)>(arg)
+using fwd(arg) = std::forward<decltype(arg)>(arg)
 
 template <class T, std::size_t N>
 constexpr std::array<std::remove_cv_t<T>, N> to_array( T (&a)[N] ){
